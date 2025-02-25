78bc02363fdfdff966738e11db824cd5a9fdd8a5
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2025-02-25 16:48:03 +0200

    add contract setup

--- contracts/ForceWithdraw.sol
+++ contracts/ForceWithdraw.sol
@@ -1,4 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ForceWithdraw {}
+import "@poolzfinance/poolz-helper-v2/contracts/interfaces/ISimpleProvider.sol";
+import "@poolzfinance/poolz-helper-v2/contracts/interfaces/ILockDealNFT.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ForceWithdraw is Ownable {
+    uint256 public immutable vaultId;
+
+    ILockDealNFT public immutable lockDealNFT;
+
+    ISimpleProvider public immutable dealProvider;
+
+    constructor(
+        ILockDealNFT _lockDealNFT,
+        uint256 _vauldId,
+        ISimpleProvider _dealProvider
+    ) Ownable(msg.sender) {
+        lockDealNFT = _lockDealNFT;
+        dealProvider = _dealProvider;
+        vaultId = _vauldId;
+    }
+
+    function forceWithdraw(address receiver) external onlyOwner {}
+}
