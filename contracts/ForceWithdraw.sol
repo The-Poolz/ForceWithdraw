078946c3571691a88b2a316c9505643f8ca21769
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2025-02-25 17:22:06 +0200

    add core logic

--- contracts/ForceWithdraw.sol
+++ contracts/ForceWithdraw.sol
@@ -2,25 +2,61 @@
 pragma solidity ^0.8.0;
 
 import "@poolzfinance/poolz-helper-v2/contracts/interfaces/ISimpleProvider.sol";
-import "@poolzfinance/poolz-helper-v2/contracts/interfaces/ILockDealNFT.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IExtendVaultManager.sol";
+import "./interfaces/IExtendLockDealNFT.sol";
 
 contract ForceWithdraw is Ownable {
-    uint256 public immutable vaultId;
+    IExtendLockDealNFT public immutable lockDealNFT;
 
-    ILockDealNFT public immutable lockDealNFT;
+    IExtendVaultManager public immutable vaultManager;
 
     ISimpleProvider public immutable dealProvider;
 
+    uint256 public immutable sourcePoolId;
+
     constructor(
-        ILockDealNFT _lockDealNFT,
-        uint256 _vauldId,
-        ISimpleProvider _dealProvider
+        IExtendLockDealNFT _lockDealNFT,
+        ISimpleProvider _dealProvider,
+        uint256 _sourcePoolId
     ) Ownable(msg.sender) {
         lockDealNFT = _lockDealNFT;
+        vaultManager = IExtendVaultManager(address(lockDealNFT.vaultManager()));
         dealProvider = _dealProvider;
-        vaultId = _vauldId;
+        sourcePoolId = _sourcePoolId;
     }
 
-    function forceWithdraw(address receiver) external onlyOwner {}
+    function forceWithdraw(
+        address receiver,
+        uint256 amount
+    ) external onlyOwner {
+        if (vaultManager.owner() != address(this)) revert InvalidOwner();
+        if (!lockDealNFT.approvedContracts(address(this)))
+            revert NotApprovedContract();
+        if (receiver == address(0)) revert ZeroAddress();
+
+        uint256 vaultId = lockDealNFT.poolIdToVaultId(sourcePoolId);
+        // activate the vault
+        vaultManager.setActiveStatusForVaultId(vaultId, true, true);
+        // mint DealProvider
+        uint256 poolId = lockDealNFT.mintForProvider(receiver, dealProvider);
+        // clone vaultId
+        lockDealNFT.cloneVaultId(poolId, sourcePoolId);
+        // register the new amount without transfer
+        uint256[] memory params = new uint256[](1);
+        params[0] = amount;
+        dealProvider.registerPool(poolId, params);
+        // return ownership to previous owner
+        vaultManager.transferOwnership(msg.sender);
+    }
+
+    function transferVaultManagerOwnership(
+        address newOwner
+    ) external onlyOwner {
+        vaultManager.transferOwnership(newOwner);
+    }
+
+    error InvalidOwner();
+    error NotApprovedContract();
+    error ZeroAddress();
 }
